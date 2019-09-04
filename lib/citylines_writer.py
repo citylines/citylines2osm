6d0f2a06d57851b2e67e9587247f58cdaf48cc43
Bruno Salerno <br.salerno@gmail.com>
2019-09-04 23:17:22 +0200

    Load ways

--- lib/citylines_writer.py
+++ lib/citylines_writer.py
@@ -7,16 +7,24 @@ class CitylinesWriter(object):
         self._nodes = 0
 
     def call(self):
+        ways = []
         for feature in self.features:
             props = feature['properties']
             coordinates = feature['geometry']['coordinates']
-            self.load_nodes(coordinates)
+            ref_ids = self.load_nodes(coordinates)
+            ways.append(osmium.osm.mutable.Way(id=-props['id'], nodes=ref_ids))
+        for way in ways:
+            self.writer.add_way(way)
 
     def load_nodes(self, coordinates):
+        refs = []
         for lonlat in coordinates:
             location = osmium.osm.Location(lonlat[0], lonlat[1])
-            node = osmium.osm.mutable.Node(id=self.node_id(), location=location)
+            id = self.node_id()
+            refs.append(id)
+            node = osmium.osm.mutable.Node(id=id, location=location)
             self.writer.add_node(node)
+        return refs
 
     def node_id(self):
         self._nodes += 1
