99c37dd8de6d53d07effb8545723906af4064e65
Bruno Salerno <br.salerno@gmail.com>
2019-09-23 19:43:59 +0200

    Simplify tests

--- test/test_parser.py
+++ test/test_parser.py
@@ -3,10 +3,6 @@ import osmium
 from lib.parser import FeaturesParser
 
 class TestFeaturesParser(unittest.TestCase):
-    SECTION_TAGS = ['citylines:id', 'network']
-    STATION_TAGS = SECTION_TAGS + ['name', 'public_transport']
-    LINE_TAGS = ['name', 'type']
-
     @classmethod
     def setUpClass(self):
         self.section = {
@@ -36,11 +32,12 @@ class TestFeaturesParser(unittest.TestCase):
         parser = FeaturesParser(features=[self.section, self.station])
         parser.run()
 
+        station_id = self.station['properties']['id']
+        section_id = self.section['properties']['id']
+
         # Nodes
         # =====
         for i, node in enumerate(parser.nodes):
-            self.assertEqual(osmium.osm.mutable.Node, node.__class__)
-
             # the two first nodes belong to the sections'way
             if i < 2:
                 self.assertEqual(self.section['geometry']['coordinates'][i], node.location)
@@ -48,40 +45,28 @@ class TestFeaturesParser(unittest.TestCase):
             # the third one belongs to the station
             else:
                 self.assertEqual(self.station['geometry']['coordinates'], node.location)
-                self.assertEqual(-self.station['properties']['id'], node.id)
+                self.assertEqual(-station_id, node.id)
 
-                # station tags
-                for i, tag in enumerate(node.tags):
-                    self.assertEqual(self.STATION_TAGS[i], tag[0])
+                expected_tags = [('citylines:id',str(station_id)),('network', 'Metro'),('name','Clot'),('public_transport','stop')]
+                self.assertEqual(expected_tags, node.tags)
 
         # Ways
         # ====
         way = parser.ways[0]
-        self.assertEqual(osmium.osm.mutable.Way, way.__class__)
-        self.assertEqual(-self.section['properties']['id'], way.id)
+        self.assertEqual(-section_id, way.id)
 
-        # section tags
-        for i, tag in enumerate(way.tags):
-            self.assertEqual(self.SECTION_TAGS[i], tag[0])
+        expected_tags = [('citylines:id', str(section_id)), ('network', 'Metro')]
+        self.assertEqual(expected_tags, way.tags)
 
-        # refs
-        refs = [-1, -2]
-        for i, ref in enumerate(way.nodes):
-            self.assertEqual(refs[i], ref)
+        self.assertEqual([-1, -2], way.nodes)
 
         # Relations
         # =========
         relation = parser.relations[0]
-        self.assertEqual(osmium.osm.mutable.Relation, relation.__class__)
         self.assertEqual(-1, relation.id)
 
-        # relation tags
-        for i, tag in enumerate(relation.tags):
-            self.assertEqual(self.LINE_TAGS[i], tag[0])
+        expected_tags = [('name', 'Metro L1'),('type', 'route')]
+        self.assertEqual(expected_tags, relation.tags)
 
-        # members
-        expected_members = [
-                ('w',-self.section['properties']['id'], ''),
-                ('n', -self.station['properties']['id'],'')
-                ]
+        expected_members = [('w',-section_id, ''),('n', -station_id,'')]
         self.assertEqual(expected_members, relation.members)
